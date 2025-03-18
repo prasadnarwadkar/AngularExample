f9bd74b5f098615d8bcc3af1dbc211abca560cad
Prasad Narwadkar <prasad.narwadkar@live.in>
2025-03-18 16:59:19 +0530

    Config changes

--- src/assets/config.js
+++ src/assets/config.js
@@ -1,5 +1,6 @@
 var config = {
-    apiBaseUrl: "http://localhost:82/api/",
+    apiBaseUrl: "http://localhost:81/api/",
+    apiBaseUrlNodeJS: "http://localhost:82/api/",
     apiBaseUrl2: "http://localhost:83/api/"
 }
 
